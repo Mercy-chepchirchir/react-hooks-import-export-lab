537b95ab142657445071cf5c0940e1f538908e72
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-20 12:26:14 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from "react";
-
+import About from "../components/About";
+import Home from "../components/Home";
+import NavBar from "../components/NavBar";
+// import image from "../components/About";
+import {username} from "../data/user";
 function App() {
+  console.log(username)
+
   return (
     <div>
       <NavBar />
